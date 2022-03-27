fd67ab345a87437c8b13aff9bb688acff487f8f4
Pank su <49202787+Cynep-SU@users.noreply.github.com>
2022-03-27 16:30:43 +0300

    Update init.el

--- init.el
+++ init.el
@@ -3,28 +3,57 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(doom-modeline org)))
+ '(custom-safe-themes
+   '("79586dc4eb374231af28bbc36ba0880ed8e270249b07f814b0e6555bdcb71fab" default))
+ '(package-selected-packages
+   '(async org-download org-attach-screenshot markdown-mode which-key dap-mode lsp-treemacs lsp-ivy helm-lsp lsp-ui lsp-mode doom-modeline org)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(global-wakatime-mode)
+
+
+
+(prefer-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+;; backwards compatibility as default-buffer-file-coding-system
+;; is deprecated in 23.2.
+(if (boundp 'buffer-file-coding-system)
+    (setq-default buffer-file-coding-system 'utf-8)
+  (setq default-buffer-file-coding-system 'utf-8))
+
+;; Treat clipboard input as UTF-8 string first; compound text next, etc.
+(setq x-select-request-type '(UTF8_STRING COMPOUND_TEXT TEXT STRING))
+
+
+
 (org-babel-do-load-languages
  'org-babel-load-languages '((C . t)))
  
- (scroll-bar-mode -1)
+(scroll-bar-mode -1)
+(tool-bar-mode -1)          ; Disable the toolbar
 
 ;; Initialize package sources
 (require 'package)
 
-(setq package-archives '(("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
                          ("org" . "https://orgmode.org/elpa/")
                          ("elpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")))
 
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
 (unless package-archive-contents
  (package-refresh-contents))
+;; Initialize use-package on non-Linux platforms
+(unless (package-installed-p 'use-package)
+   (package-install 'use-package))
 
 
 (require 'use-package)
@@ -50,15 +79,12 @@
          ("C-d" . ivy-reverse-i-search-kill))
   :config
   (ivy-mode 1))
-  
-(use-package elpy
-  :ensure t
-  :init
-  (elpy-enable))
+ 
+
 
 
 (setq org-odt-preferred-output-format "docx")
-
+(setq inhibit-compacting-font-caches t)
 (use-package doom-modeline
   :ensure t
   :init (doom-modeline-mode 1)
@@ -71,3 +97,48 @@
 
 
 (setq dashboard-startup-banner "~/me/god.png")
+
+(use-package markdown-mode
+  :ensure t
+  :mode ("README\\.md\\'" . gfm-mode)
+  :init (setq markdown-command "multimarkdown"))
+
+(autoload 'markdown-mode "markdown-mode"
+   "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist
+             '("\\.\\(?:md\\|markdown\\|mkd\\|mdown\\|mkdn\\|mdwn\\)\\'" . markdown-mode))
+
+(defun my-org-screenshot ()
+  "Take a screenshot into a time stamped unique-named file in the
+same directory as the org-buffer and insert a link to this file."
+  (interactive)
+  (setq filename
+        (concat
+         (make-temp-name
+          (concat (buffer-file-name)
+                  "_"
+                  (format-time-string "%Y%m%d_%H%M%S_")) ) ".png"))
+  (call-process "magick" nil nil nil "clipboard:" "\""  filename "\"")
+  (insert (concat "[[" filename "]]"))
+  (org-display-inline-images))
+
+(use-package org-download
+    :after org
+    :defer nil
+    :custom
+    (org-download-method 'directory)
+    (org-download-image-dir "images")
+    (org-download-heading-lvl nil)
+    (org-download-timestamp "%Y%m%d-%H%M%S_")
+    (org-image-actual-width 300)
+    (org-download-screenshot-method "magick clipboard: \"%s\"")
+    :bind
+    ("C-M-y" . org-download-screenshot)
+    :config
+    (require 'org-download))
+
+(require 'org-download)
+
+;; Drag-and-drop to `dired`
+(add-hook 'dired-mode-hook 'org-download-enable)
+  
