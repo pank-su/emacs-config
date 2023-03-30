a4b641697681e45b9c4dee0a2f169af11f2c72b0
pank-su <frostkslo1@yandex.ru>
2023-03-30 20:31:22 +0300

    Обновление конфига

--- init.el
+++ init.el
@@ -15,7 +15,8 @@
 
 (setq package-archives '(("melpa" . "https://raw.githubusercontent.com/d12frosted/elpa-mirror/master/melpa/")
                          ("org" . "https://orgmode.org/elpa/")
-                         ("elpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")))
+                         ("elpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
+			 ("gnu-devel" . "https://elpa.gnu.org/devel/")))
 
 (package-initialize)
 (unless package-archive-contents
@@ -27,24 +28,6 @@
 
 (require 'use-package)
 
-(put 'use-package 'lisp-indent-function 1)
-
-(use-package use-package-core
-  :custom
-  ;; (use-package-verbose t)
-  ;; (use-package-minimum-reported-time 0.005)
-  (use-package-enable-imenu-support t))
-
-(use-package gcmh
-  :ensure t
-  :demand t
-  :config
-  (gcmh-mode 1))
-
-(use-package system-packages
-  :ensure t
-  :custom
-  (system-packages-noconfirm t))
 
 (use-package use-package-ensure-system-package :ensure t)
 
@@ -56,8 +39,6 @@
 (add-hook 'org-mode-hook #'org-modern-mode)
 (add-hook 'org-agenda-finalize-hook #'org-modern-agenda)
 
-
-
 ;; (use-package dracula-theme)
 (set-face-attribute 'default nil :font "JetBrains Mono" :height 100)
 ;; (load-theme 'dracula t)
@@ -67,16 +48,53 @@
 
 (org-babel-do-load-languages
  'org-babel-load-languages
- '((plantuml . t)))
+ '(
+   (sql . t)
+   (java . t)
+   (kotlin . t)
+   (shell . t)
+   (plantuml . t)
+  )
+ )
 
 (setq org-plantuml-jar-path "~/.emacs.d/plantuml.jar")
 
+(setq org-latex-classes
+   '(("extarticle" "\\documentclass[14pt]{extarticle}"
+	  ("\\section{%s}" . "\\section*{%s}")
+	  ("\\subsection{%s}" . "\\subsection*{%s}")
+	  ("\\subsubsection{%s}" . "\\subsubsection{%s}")
+	  ("\\paragraph{%s}" . "\\paragraph*{%s}")
+	  ("\\subparagraph{%s}" . "\\subparagraph*{%s}"))
+	 ("article" "\\documentclass[11pt]{article}"
+	  ("\\section{%s}" . "\\section*{%s}")
+	  ("\\subsection{%s}" . "\\subsection*{%s}")
+	  ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+	  ("\\paragraph{%s}" . "\\paragraph*{%s}")
+	  ("\\subparagraph{%s}" . "\\subparagraph*{%s}"))
+	 ("report" "\\documentclass[11pt]{report}"
+	  ("\\part{%s}" . "\\part*{%s}")
+	  ("\\chapter{%s}" . "\\chapter*{%s}")
+	  ("\\section{%s}" . "\\section*{%s}")
+	  ("\\subsection{%s}" . "\\subsection*{%s}")
+	  ("\\subsubsection{%s}" . "\\subsubsection*{%s}"))
+	 ("book" "\\documentclass[11pt]{book}"
+	  ("\\part{%s}" . "\\part*{%s}")
+	  ("\\chapter{%s}" . "\\chapter*{%s}")
+	  ("\\section{%s}" . "\\section*{%s}")
+	  ("\\subsection{%s}" . "\\subsection*{%s}")
+	  ("\\subsubsection{%s}" . "\\subsubsection*{%s}"))))
 
+(setq
+   org-export-in-background t
+   org-latex-minted-options '(("breaklines" "true")
+	 ("float" "t")
+	 ("breakanywhere" "true")
+	 ("fontsize" "\\footnotesize"))
+    org-latex-default-table-environment "longtable")
 
-
-(use-package ox-reveal)
-
-(setq org-reveal-root "https://cdn.jsdelivr.net/npm/reveal.js")
+(use-package ox-reveal
+  :config (setq org-reveal-root "https://cdn.jsdelivr.net/npm/reveal.js"))
 
 
 
@@ -121,7 +139,10 @@
 (defun my-latex-filter-continue-string (text backend info)
   "Ensure \"_\" are properly handled in LaTeX export."
   (when (org-export-derived-backend-p backend 'latex)
-      (string-replace "\\\\" "\\\\ \n \\hline" (replace-regexp-in-string "Continued on next page" "Продолжение на следующей странице" (replace-regexp-in-string "Continued from previous page" "Продолжение с предыдущей страницы" text))))
+    (string-replace "\\\\" "\\\\ \n \\hline" (replace-regexp-in-string "Continued on next page"
+								       "Продолжение на следующей странице"
+								       (replace-regexp-in-string "Continued from previous page"
+												 "Продолжение с предыдущей страницы" text))))
   )
 
 
@@ -137,8 +158,6 @@
         "pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"
 	"pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"))
 
-(setq org-latex-minted-options '(("breaklines" "true")
-                                 ("breakanywhere" "true")))
 
 
 
@@ -150,52 +169,16 @@
     (org-download-method 'directory)
     (org-download-image-dir "images")
     (org-download-heading-lvl nil)
-    (org-download-timestamp "%Y%m%d-%H%M%S_")
+    ;; (org-download-timestamp "%Y%m%d-%H%M%S_")
     (org-image-actual-width 300)
     (org-download-screenshot-method "magick clipboard: \"%s\"")
     :bind
     ("C-M-y" . org-download-screenshot)
     :config
-    (require 'org-download))
+    (require 'org-download)
+    (add-hook 'dired-mode-hook 'org-download-enable)
+    )
 
-
-(require 'org-download)
-(add-hook 'dired-mode-hook 'org-download-enable)
-
-(org-babel-do-load-languages
- 'org-babel-load-languages
- '((sql . t)
-   (java . t)
-   (kotlin . t)
-   (shell . t)
-   ))
-
-
-
-(use-package activity-watch-mode)
-
-(global-activity-watch-mode)
-
-;; My library
-;; (use-package notion-org
-  ;; :load-path "C:/Users/user/Desktop/notion-org/")
-
-(use-package company
-  :config (global-company-mode))
-
-(use-package dashboard
-  :ensure t
-  :config
-  (dashboard-setup-startup-hook))
-
-
-(setq dashboard-startup-banner "~/.emacs.d/me/god.png")
-
-(use-package magit)
-
-;; (use-package activity-watch-mode)
-
-;; (global-activity-watch-mode)
 
 ;; My library
 ;; (use-package notion-org
@@ -234,32 +217,6 @@
 (use-package company-box
   :hook (company-mode . company-box-mode))
 
-(use-package tree-sitter)
-(use-package tree-sitter-langs)
-
-(global-tree-sitter-mode)
-
-(use-package lsp-mode
-  :init
-  ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
-  (setq lsp-keymap-prefix "C-c l")
-  :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
-         (python-mode . lsp)
-	 (java-mode . lsp)
-	 (kotlin-mode . lsp)
-         )
-  :commands lsp)
-
-(use-package flycheck)
-
-(use-package lsp-java :config (add-hook 'java-mode-hook 'lsp))
-
-;; optionally
-(use-package lsp-ui :commands lsp-ui-mode)
-
-(use-package lsp-treemacs :commands lsp-treemacs-symbols)
-
-
 (use-package google-translate)
 
 
@@ -268,57 +225,22 @@
 (global-set-key "\C-ct" 'google-translate-smooth-translate)
 (setq google-translate-default-target-language "ru") 
 
-(defun my-org-confirm-babel-evaluate (lang body)
-  (not (string= lang "sql")))  ;don't ask for ditaa
-(setq org-confirm-babel-evaluate #'my-org-confirm-babel-evaluate)
-
-(setq org-babel-default-header-args:sql '(
-					  (:dbuser . "org-mode")
-					  (:dbpassword . "org-mode")
-					  ))
-
-
 ;; (setq org-latex-image-default-option '(("float" "wrap")))
 
-(global-tree-sitter-mode)
+(use-package org-ai
+  :load-path "./org-ai"
+  :commands (org-ai-mode)
+  :custom
+  (org-ai-openai-api-token "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")
+  :init
+  (add-hook 'org-mode-hook #'org-ai-mode)
+  )
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-export-in-background t)
- '(org-latex-classes
-   '(("extarticle" "\\documentclass[14pt]{extarticle}"
-	  ("\\section{%s}" . "\\section*{%s}")
-	  ("\\subsection{%s}" . "\\subsection*{%s}")
-	  ("\\subsubsection{%s}" . "\\subsubsection{%s}")
-	  ("\\paragraph{%s}" . "\\paragraph*{%s}")
-	  ("\\subparagraph{%s}" . "\\subparagraph*{%s}"))
-	 ("article" "\\documentclass[11pt]{article}"
-	  ("\\section{%s}" . "\\section*{%s}")
-	  ("\\subsection{%s}" . "\\subsection*{%s}")
-	  ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
-	  ("\\paragraph{%s}" . "\\paragraph*{%s}")
-	  ("\\subparagraph{%s}" . "\\subparagraph*{%s}"))
-	 ("report" "\\documentclass[11pt]{report}"
-	  ("\\part{%s}" . "\\part*{%s}")
-	  ("\\chapter{%s}" . "\\chapter*{%s}")
-	  ("\\section{%s}" . "\\section*{%s}")
-	  ("\\subsection{%s}" . "\\subsection*{%s}")
-	  ("\\subsubsection{%s}" . "\\subsubsection*{%s}"))
-	 ("book" "\\documentclass[11pt]{book}"
-	  ("\\part{%s}" . "\\part*{%s}")
-	  ("\\chapter{%s}" . "\\chapter*{%s}")
-	  ("\\section{%s}" . "\\section*{%s}")
-	  ("\\subsection{%s}" . "\\subsection*{%s}")
-	  ("\\subsubsection{%s}" . "\\subsubsection*{%s}"))))
- '(org-latex-default-table-environment "longtable")
- '(org-latex-minted-options
-   '(("breaklines" "true")
-	 ("float" "t")
-	 ("breakanywhere" "true")
-	 ("fontsize" "\\footnotesize")))
  '(package-selected-packages
    '(gradle-mode csv-mode htmlize csharp-mode magit dashboard company activity-watch-mode org-download ox-reveal dracula-theme org-modern use-package-ensure-system-package system-packages gcmh use-package))
  '(warning-suppress-log-types '((comp))))
